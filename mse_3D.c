10aca1a20a28332c5cffb333de5025dabcc3f53d
bcm <bcmardini@gmail.com>
2023-05-06 13:24:42 -0400

    c_mse_3D.1

---
+++ mse_3D.c
@@ -0,0 +1,210 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+double*** read_csv(const char* folder_path, int num_files, int rows, int cols) {
+    double*** data_array = (double***)malloc(num_files * sizeof(double**));
+
+    // Iterate through all files in the folder
+    DIR* dir;
+    struct dirent* ent;
+    if ((dir = opendir(folder_path)) != NULL) {
+        int file_count = 0;
+        while ((ent = readdir(dir)) != NULL) {
+            // Only process CSV files
+            if (strstr(ent->d_name, ".csv") != NULL) {
+                char file_path[1024];
+                snprintf(file_path, sizeof(file_path), "%s/%s", folder_path, ent->d_name);
+                FILE* file = fopen(file_path, "r");
+                if (file == NULL) {
+                    printf("Could not open file: %s\n", file_path);
+                    return NULL;
+                }
+                char line[1024];
+
+                // Allocate memory for the data array
+                double** data = (double**)malloc(rows * sizeof(double*));
+                for (int i = 0; i < rows; i++) {
+                    data[i] = (double*)malloc(cols * sizeof(double));
+                }
+
+                // Read the data into the array
+                int row = 0;
+                while (fgets(line, 1024, file)) {
+                    int col = 0;
+                    char* token = strtok(line, ",");
+                    while (token != NULL) {
+                        data[row][col] = atof(token);
+                        col++;
+                        token = strtok(NULL, ",");
+                    }
+                    row++;
+                }
+                fclose(file);
+                data_array[file_count++] = data;
+            }
+        }
+        closedir(dir);
+    } else {
+        printf("Could not open folder: %s\n", folder_path);
+        return NULL;
+    }
+    return data_array;
+}
+
+double*** coarse_graining(double*** list_of_matrices, int num_matrices, int scale, int rows, int cols) {
+    if (scale == 1){
+        return list_of_matrices;
+    }
+    int new_num_matrices = num_matrices / scale;
+    double*** new_list_of_matrices = (double***)malloc(new_num_matrices * sizeof(double**));
+    for (int i = 0; i < new_num_matrices; i++) {
+        new_list_of_matrices[i] = (double**)malloc(sizeof(double*) * rows);
+        for (int j = 0; j < rows; j++) {
+            new_list_of_matrices[i][j] = (double*)malloc(sizeof(double) * cols);
+            for (int k = 0; k < cols; k++) {
+                double sum = 0.0;
+                for (int s = i * scale; s < (i + 1) * scale && s < num_matrices; s++) {
+                    sum += list_of_matrices[s][j][k];
+                }
+                new_list_of_matrices[i][j][k] = (int)round(sum / scale);
+            }
+        }
+    }
+    return new_list_of_matrices;
+}
+
+
+double max_distance(int m, double ***list_of_matrices, int i, int j, int k, int a, int b, int c) {
+    double max_dist = 0;
+    for (int n = 0; n < m; n++){
+        for(int z = 0; z < m; z++) {
+            for(int l = 0; l < m; l++) {
+                double dist = fabs(list_of_matrices[k+n][i+z][j+l] - list_of_matrices[c+n][a+z][b+l]);
+                if(dist > max_dist) {
+                    max_dist = dist;
+                }
+            }
+        }
+    }
+    return max_dist;
+}
+
+float calculate_U_ij_m(double ***list_of_matrices, int i, int j, int k, int m, double r, int H, int W, int T) {
+    int count = 0;
+    int N_m = (H - m) * (W - m) * (T - m);
+    for (int c = 0; c < T - m; c++){
+        for (int a = 0; a < H - m; a++) {
+            for (int b = 0; b < W - m; b++) {
+                if (a == i && b == j && c == k) {
+                    continue;
+                }
+                if (max_distance(m, list_of_matrices, i, j, k, a, b, c) <= r) {
+                    count++;
+                }
+            }
+        }
+    }
+    return (float) count / (N_m-1);
+}
+
+float calculate_U_ij_m_plus_one(double ***list_of_matrices, int i, int j, int k, int m, double r, int H, int W, int T) {
+    int count = 0;
+    int N_m = (H - m) * (W - m) * (T - m);
+    for (int c = 0; c < T - m; c++){
+        for (int a = 0; a < H - m; a++) {
+            for (int b = 0; b < W - m; b++) {
+                if (a == i && b == j && c == k) {
+                    continue;
+                }
+                if (max_distance(m, list_of_matrices, i, j, k, a, b, c) <= r) {
+                    count++;
+                }
+            }
+        }
+    }
+    return (float) count / (N_m-1);
+}
+
+float calculate_U_m(double ***list_of_matrices, int m, double r, int H, int W, int T) {
+    float sum = 0.0;
+    for (int k = 0; k < T - m; k++){
+        for (int i = 0; i < H - m; i++) {
+            for (int j = 0; j < W - m; j++) {
+                sum += calculate_U_ij_m(list_of_matrices, i, j, k, m, r, H, W, T);
+            }
+        }
+    }
+    return sum / ((H - m) * (W - m) * (T - m));
+}
+
+float calculate_U_m_plus_one(double ***list_of_matrices, int m, double r, int H, int W, int T) {
+    m += 1;
+    float sum = 0.0;
+    for (int k = 0; k < T - m; k++){
+        for (int i = 0; i < H - m; i++) {
+            for (int j = 0; j < W - m; j++) {
+                sum += calculate_U_ij_m_plus_one(list_of_matrices, i, j, k, m, r, H, W, T);
+            }
+        }
+    }
+    return sum / ((H - m) * (W - m) * (T - m));
+}
+
+float negative_logarithm(float um, float umplus1) {
+    if (um == 0) {
+        return 0;
+    }
+    else {  
+        float result = -log(umplus1 / um);
+        return result;  
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    char* file_path = argv[1];
+    int num_files = atoi(argv[2]);
+    int scales = atoi(argv[3]);
+    int rows = atoi(argv[4]);
+    int cols = atoi(argv[5]);
+    int m = atoi(argv[6]);
+    int r = atoi(argv[7]);
+
+    double*** list_of_matrices = read_csv(file_path, num_files, rows, cols);
+    double* n_values = malloc(scales * sizeof(double));
+
+    for (int i = 1; i <= scales; i++) {
+        double*** coarse_data = coarse_graining(list_of_matrices, num_files, i, rows, cols);
+        float U_m = calculate_U_m(coarse_data, m ,r, cols, rows, num_files/i);
+        float U_m_plus_one = calculate_U_m_plus_one(coarse_data, m, r, cols, rows, num_files/i);
+        float n = negative_logarithm(U_m, U_m_plus_one);
+        n_values[i-1] = n;
+    }
+
+    for (int i = 0; i < scales; i++) {
+        printf("%f ", n_values[i]);
+    }
+    printf("\n");
+
+    // double*** coarse_data = coarse_graining(list_of_matrices, num_files, 20, rows, cols);
+    // for (int i = 0; i < num_files/20; i++) {
+    //     printf("Matrix %d:\n", i+1);
+    //     for (int j = 0; j < rows; j++) {
+    //         for (int k = 0; k < cols; k++) {
+    //             printf("%lf ", coarse_data[i][j][k]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     printf("\n");
+    // }
+
+    return 0;
+}
+
+/*
+gcc mse_3D.c -o mse_3D -lm
+./mse_3D
+*/
---
+++ mse_3D.py
@@ -0,0 +1,88 @@
+import subprocess
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import os
+import pandas as pd
+import time
+
+def plot_arrays(data_list, title='', xlabel='', ylabel='', legends=None, save_path=None):
+    fig, ax = plt.subplots()
+    plt.grid('black')
+    ax.set_facecolor('lightgrey')
+    for i, data_tuple in enumerate(data_list):
+        name, data = data_tuple
+        if legends is not None and len(legends) == len(data_list):
+            label = legends[i]
+        else:
+            label = name
+        plt.plot(data, color=f'C{i}', marker='v', markersize=5, label=label, markeredgecolor='black')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+def calculate_csv_std(folder_path):
+    unique_values = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(folder_path, filename)
+            df = pd.read_csv(file_path, header=None)
+            unique_values.extend(np.unique(df.values.astype(float)))
+    unique_values = np.array(unique_values).astype(float)
+    return np.std(unique_values)
+
+
+
+def run_c_program(csv_path, scales, m, r):
+    num_files = len([f for f in os.listdir(csv_path) if f.endswith('.csv')])
+    csv_file = os.path.join(csv_path, os.listdir(csv_path)[0])
+    with open(csv_file, 'r') as f:
+        reader = csv.reader(f)
+        csv_rows = len(list(reader))
+        f.seek(0)
+        csv_cols = len(next(reader))
+
+    r_std = calculate_csv_std(csv_path) * r
+    command = ['./mse_3D', csv_path, str(num_files), str(scales), str(csv_rows), str(csv_cols), str(m), str(r_std)]
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8').strip()
+    n_values = list(output.split())
+    n_values = [float(x) for x in n_values]
+    return n_values
+
+
+def mse_3D(folder_path, scales, m, r):
+    mse_values = []
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        print('Working on: ', filename)
+        start_time = time.time()
+
+        mse_values.append([filename, run_c_program(file_path, scales, m, r)])
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(execution_time)
+
+    return mse_values
+
+
+# Ejemplo:
+
+# 'Datos/10x10x100/datos_csv' => Hay que pasarle el path de la carpeta en donde estan los .csv de los datos originales (Hay que crear los .csv para cada imagen)
+# (Es cosa de pedirle a chat gpt que haga una funcion que haga esto)
+
+# 20 => escalas
+# 1 => m
+# 0.5 => r
+
+# Tambien imprime el tiempo en segundos que se demora.
+
+# En el ejemplo de abajo son 100 frames chicas de 10x10, por eso hay que ponerle m=1 y r=0.5.
+
+# v = mse_3D('Datos/10x10x100/datos_csv', 20, 1, 0.5)
+# plot_arrays(v)
