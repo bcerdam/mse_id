5fbf5eebea406d7cc992e9c0d245d7a4c0644f75
bcerdam <bcmardini@gmail.com>
2024-02-27 15:35:25 -0300

    test

--- core_py/mse_3d/cluster_functions.py
+++ core_py/mse_3d/cluster_functions.py
@@ -33,3 +33,47 @@ def download_folder_from_server(username, ssh_path, password, remote_folder, loc
         print(f"Folder '{remote_folder}' downloaded to '{local_folder}'")
     except Exception as e:
         print(f"Error: {e}")
+
+
+'''
+upload_folder_to_server(): Uploads CONTENT of local folder to a given path (folder) on the cluster.
+    - username: Username for SSH connection.
+    - ssh_path: SSH address of the cluster.
+    - password: Password for SSH connection.
+    - local_folder: Path of the local folder that has the CONTENT to upload.
+    - remote_path: Path of folder where to upload the CONTENTS on the cluster.
+'''
+
+
+def upload_folder_to_server(username, ssh_path, password, local_folder, remote_path):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh_client.connect(hostname=ssh_path, username=username, password=password)
+        sftp_client = ssh_client.open_sftp()
+
+        # Create the remote directory if it doesn't exist
+        try:
+            sftp_client.chdir(remote_path)
+        except IOError:
+            sftp_client.mkdir(remote_path)
+            sftp_client.chdir(remote_path)
+
+        # Upload the entire folder recursively
+        for root, dirs, files in os.walk(local_folder):
+            for file in files:
+                local_file = os.path.join(root, file)
+                remote_file = os.path.join(remote_path, os.path.relpath(local_file, local_folder))
+                sftp_client.put(local_file, remote_file)
+
+        sftp_client.close()
+        ssh_client.close()
+
+        print(f"Folder '{local_folder}' uploaded to '{remote_path}' on the cluster")
+    except Exception as e:
+        print(f"Error: {e}")
+
+        print(f"Folder '{local_folder}' uploaded to '{remote_path}' on the cluster")
+    except Exception as e:
+        print(f"Error: {e}")
